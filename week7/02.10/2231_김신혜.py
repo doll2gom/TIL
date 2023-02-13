648f3b663923d212a63c311a07aef3874cb5c88c
doll2gom <shkim_ok@naver.com>
2023-02-13 16:59:28 +0900

    Update 02.10,02.11

--- week7/02.10/2231_김신혜.py
+++ week7/02.10/2231_김신혜.py
@@ -18,46 +18,52 @@ A(생성자) <- N
 첫째 줄에 답을 출력한다. 생성자가 없는 경우에는 0을 출력한다.'''
 """216"""
 #198
-
-
 '''
 100의 자리의 경우 
 숫자의 길이는 3이되고 각 자리마다 9가 들어갔을 때 최댓값을 제외하여 계산하기로 한다. 
 999 (999+9+9+9)
 '''
-lst = []    # 분해합 후보 리스트
-lst2 = []   # 분해합에 대당하는 리스트
+'''from collections import deque
+lst = deque()    # 분해합 후보 리스트
+lst2 = deque()   # 분해합에 대당하는 리스트
 N = int(input())
-length = len(str(N))    # 숫자의 길이를 구한다.
+for _ in range(N):
+    # 탐색할 정수(생성자 후보)를 10으로 나누면서 몫과 나머지를 구한다.
+    lst.append(N)
+    a = divmod(N, 10)
+    if a[0] == 0:
+        break
+    lst.append(a)
+    print(a)
+    a = a[0]
+    # 나머지를 리스트에 담는다.
+    # 몫은 다시 탐색할 숫자로 변경
 
-# 숫자가 100의 자리 이상일 경우를 고려해
-# 각 자리에 9가 들어갔을 때의 최댓값 만큼 경우를 탐색
-for n in range(1,length*9+1):   
-
-    # 원래 입력받은 분해합 정수에서 1씩 줄여가면서 탐색
-    number = N - n
-
-    # 먼저 리스트에 탐색할 정수(생성자 후보)를 담는다.
-    lst.append(number)
-
-    while True:
-        # 탐색할 정수(생성자 후보)를 10으로 나누면서 몫과 나머지를 구한다.
-        a = divmod(number, 10)
-        # 나머지를 리스트에 담는다.
-        lst.append(a[1])
-        # 몫은 다시 탐색할 숫자로 변경
-        number = a[0]
-
-        # 몫이 0이 될때까지 반복하여 나머지를 계속해서 리스트에 모은다.
-        # 몫이 0이 나오면 break 
-        if a[0] == 0:
-            break
+    # 몫이 0이 될때까지 반복하여 나머지를 계속해서 리스트에 모은다.
+    # 몫이 0이 나오면 break 
     
     # lst 속의 숫자의 합이 원래 입력값과 완전히 동일하다면 
     # 가장 앞에 있는 인덱스[0]의 수가 생성자에 해당하기 때문에 두번째 리스트에 모아둔다.
-    if sum(lst) == N:
-        lst2.append(lst[0])
-        print('3 : ', n, lst)
-    
-    lst.clear() # 탐색이 끝나면 lst를 비운다.
-print(min(lst2))    # 두번째 리스트에서 가장 작은 생성자를 출력
+        # print('3 : ', n, lst)
+
+print(sorted(lst2)[0])    # 두번째 리스트에서 가장 작은 생성자를 출력'''
+
+import sys
+from collections import deque
+lst = deque()
+N = int(sys.stdin.readline())
+for n in range(N,1,-1):   
+    for _ in n:
+        print(n)
+        cnt = [n]
+        n1 = n//10
+        if n1 == 0:
+            break
+        n2 = n%10
+        print(n1, n2)
+        cnt.append(n2)
+        n = n1
+    if sum(cnt) == N:
+        lst.append(cnt[0])
+    print(sorted(lst))
+# print(sorted(lst2)[0]) 
